1b08ad4a0446ab6301a15dac92698e6784a8cab6
“Katya <katjasaveleva@gmail.com>
2020-08-25 22:34:37 +0200

    cleanup

--- src/pdtb/preprocess_pdtb_xlnet.py
+++ src/pdtb/preprocess_pdtb_xlnet.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
 
+import glob
 import os
-from argparse import ArgumentParser
-from collections import Counter
 import random
 
-
-def get_file_paths(data_dir):
-    # Get paths for all files in the given directory
-
-    file_names = []
-    # r=root, d=directories, f = files
-    for r, d, f in os.walk(args.data):
-        for file in f:
-            if '.pipe' in file:
-                file_names.append(os.path.join(r, file))
-    return file_names
+from argparse import ArgumentParser
+from collections import Counter
 
 
 def split_data(data):
 
-    splitted_data = data.rstrip('\n').split('|')
-    return splitted_data
+    split_data = data.rstrip('\n').split('|')
+    return split_data
 
 
 def preprocess_disc_rel(discourse_rel, l1=True, l2=False):
@@ -39,6 +29,7 @@ def preprocess_disc_rel(discourse_rel, l1=True, l2=False):
 
 
 def read_file(file, pdtb=False):
+    '''Parse pipe files'''
 
     preprocessed_lines = []
     with open(file, 'r', encoding='utf-8', errors='ignore') as fd:
@@ -46,28 +37,31 @@ def read_file(file, pdtb=False):
         for item in content:
             item = split_data(item)
             if pdtb:
-                sec_num = item[1] # Section number (folder name)
-                file_num = item[2] # File number
+                sec_num = item[1]  # Section number (folder name)
+                file_num = item[2]  # File number
             else:
+                # extract section number and file id from file name
                 file_name = os.path.basename(file)
                 file_name = os.path.splitext(file_name)[0]
                 file_name = os.path.splitext(file_name)[0]
                 file_name = file_name.replace('wsj_', '')
                 sec_num = file_name[:2]
                 file_num = file_name[-2:]
-            rel_type = item[0] # Relation type
-            text1 = item[24] # Arg1 text
-            text2 = item[34] # Arg2 text       
+            rel_type = item[0]  # Relation type
+            text1 = item[24]  # Arg1 text
+            text2 = item[34]  # Arg2 text
 
-            if rel_type == "Implicit" or rel_type == "Explicit" or rel_type == "AltLex" or rel_type == "EntRel":
-                #disc_rel = preprocess_disc_rel(item[11], l2=True)
+            if (rel_type == "Implicit" or
+                    rel_type == "Explicit" or
+                    rel_type == "AltLex" or rel_type == "EntRel"):
+                # disc_rel = preprocess_disc_rel(item[11], l2=True)
                 # second optional field for the Implicit, Explicit and AltLex: record[13]
-                line = '{}\t{}\t{}\t{}\t{}\t{}\n'.format(sec_num, file_num, rel_type, "Rel", text1, text2)
-                preprocessed_lines.append(line)
+                rel = "Rel"
             else:
-                print('jjjjjjjjjjjjjj')
-                line = '{}\t{}\t{}\t{}\t{}\t{}\n'.format(sec_num, file_num, rel_type, "NoRel", text1, text2)
-                preprocessed_lines.append(line)
+                rel = "NoRel"
+            line = '{}\t{}\t{}\t{}\t{}\t{}\n'.format(
+                sec_num, file_num, rel_type, rel, text1, text2)
+            preprocessed_lines.append(line)
 
     return preprocessed_lines
 
@@ -76,7 +70,6 @@ def split_corpus(corpus, pdtb=True):
     '''Split preprocessed corpus into the train, dev and test sets'''
 
     train_set = []
-    #dev_set = []
     test_set = []
 
     for line in corpus:
@@ -86,16 +79,12 @@ def split_corpus(corpus, pdtb=True):
             train_set.append(line)
         elif section in range(23, 25):
             test_set.append(line)
-        # No dev set so far
-        #elif section in range(0, 2):
-        #    dev_set.append(line)
 
-    #return train_set, dev_set, test_set
     return train_set, test_set
 
 
 def count_senses(corpus):
-    
+
     sense_list = [line.split('\t')[3] for line in corpus]
     sense_count = Counter(sense_list)
 
@@ -106,25 +95,23 @@ def count_senses(corpus):
 
 
 def delete_infrequent(corpus, sense_count, train=False):
+    '''Remove sense if there are not enough training examples'''
 
     labels = []
-    print(len(corpus))
     for line in corpus[:]:
         disc_rel = line.split('\t')[3]
         disc_rel_split = disc_rel.split('.')
         if (sense_count[disc_rel] < 50 or
-            len(disc_rel_split) == 1):
+                len(disc_rel_split) == 1):
             corpus.remove(line)
         else:
             labels.append(disc_rel)
 
-    if train:
-        print(Counter(labels))
-    
     return corpus, set(labels)
 
 
 def handle_imbalance(training_set):
+    '''Upsample NoRel class'''
 
     args1 = []
     args2 = []
@@ -142,37 +129,28 @@ def handle_imbalance(training_set):
         if rel == 'NoRel':
             norel_items.append(item)
 
-    # add items to the non-rel class by randomly combining statements 
+    # add items to the non-rel class by randomly combining statements
     assert len(args1) == len(args2)
 
     imbalance_count = len(args1) - len(norel_items)
-    #args1_sampling = random.choices(args1, k=imbalance_count)
-    args1_sampling_indicies = random.choices(range(len(args1)), k=int(imbalance_count * 0.006))
+    args1_sampling_indicies = random.choices(
+        range(len(args1)), k=int(imbalance_count * 0.006))
+
     for i, arg1_i in enumerate(args1_sampling_indicies):
         print('{} out of {} processed'.format(i, len(args1_sampling_indicies)))
         # assert we don't pick the corresponding pair
         arg1 = args1[arg1_i]
-        #args2_exlude = args2[1:arg1_i] + args2[arg1_i+1:]
         args2_exlude = args2.copy()
         del args2_exlude[arg1_i]
         args2_random = random.choice(args2_exlude)
-        line = '{}\t{}\t{}\t{}\t{}\t{}\n'.format("generated", "generated", "generated", "NoRel", arg1, args2_random)
+        line = '{}\t{}\t{}\t{}\t{}\t{}\n'.format(
+            "generated", "generated", "generated", "NoRel", arg1, args2_random)
         norel_items.append(line)
         generated.append(line)
-
-    #assert len(args1) == len(norel_items)
 
     balanced = training_set + generated
 
     return balanced
-
-
-def write_to_file(corpus, name):
-
-    with open(name, 'w') as file:
-        for line in corpus:
-            file.write(line)
-        file.close()
 
 
 if __name__ == '__main__':
@@ -183,39 +161,40 @@ if __name__ == '__main__':
         'data', help='Path to the directory containing the data')
     args = parser.parse_args()
 
-    file_paths = get_file_paths(args.data)
+    files = glob.glob(
+        '{}/*.pipe'.format(args.data), recursive=True)
 
     preprocessed_corpus = []
 
-    for file in file_paths:
+    for file in files:
         new_lines = read_file(file)
         preprocessed_corpus += new_lines
 
-    #train_set, dev_set, test_set = split_corpus(preprocessed_corpus)
-    #train_set, test_set = split_corpus(preprocessed_corpus)
+    # train_set, dev_set, test_set = split_corpus(preprocessed_corpus)
+    # train_set, test_set = split_corpus(preprocessed_corpus)
 
-    #sense_count, relation_count = count_senses(train_set)
+    # sense_count, relation_count = count_senses(train_set)
 
     balanced_train_set = handle_imbalance(preprocessed_corpus)
-    print(len(balanced_train_set))
     random.shuffle(balanced_train_set)
-    print(balanced_train_set)
-    #train_set, labels_train = delete_infrequent(train_set, sense_count, train=True)
-    #dev_set, labels_dev = delete_infrequent(dev_set, sense_count)
-    #test_set, labels_test = delete_infrequent(test_set, sense_count)
+    # train_set, labels_train = delete_infrequent(train_set, sense_count, train=True)
+    # dev_set, labels_dev = delete_infrequent(dev_set, sense_count)
+    # test_set, labels_test = delete_infrequent(test_set, sense_count)
 
     # print(len(train_set), len(dev_set), len(test_set))
-    #print(len(train_set), len(test_set))
+    # print(len(train_set), len(test_set))
 
     sense_count_tr, relation_count = count_senses(balanced_train_set)
-    #sense_count_tr_bal, relation_count = count_senses(balanced_train_set)
-    #sense_count_dev, relation_count = count_senses(dev_set)
-    #sense_count_ts, relation_count = count_senses(test_set)
+    # sense_count_tr_bal, relation_count = count_senses(balanced_train_set)
+    # sense_count_dev, relation_count = count_senses(dev_set)
+    # sense_count_ts, relation_count = count_senses(test_set)
 
-   # assert labels_train == labels_dev == labels_test
+    # assert labels_train == labels_dev == labels_test
 
-    write_to_file(balanced_train_set, 'ptb_dev.txt')
-    #write_to_file(balanced_train_set, 'ptb_train_bal.txt')
-    #write_to_file(dev_set, 'pdtb_dev.txt')
-    #write_to_file(test_set, 'ptb_test.txt')
-    #print(len(balanced_train_set))
+    with open('ptb_dev.txt', "w") as fd:
+        fd.write("\n".join(balanced_train_set))
+
+    # write_to_file(balanced_train_set, 'ptb_train_bal.txt')
+    # write_to_file(dev_set, 'pdtb_dev.txt')
+    # write_to_file(test_set, 'ptb_test.txt')
+    # print(len(balanced_train_set))
